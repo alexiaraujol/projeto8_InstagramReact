79fd621923130253ba452593f71161a2f29eb975
alexiaraujol <151393882+alexiaraujol@users.noreply.github.com>
2024-12-16 16:17:58 -0300

    meu primeiro site em react

---
+++ projeto-instagram-react/src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar";
+import Body from "./Body";
+
+
+export default function App() {
+
+    return (
+        <div>
+
+            <Navbar />
+
+            <Body />
+
+
+        </div>
+
+    );
+}
