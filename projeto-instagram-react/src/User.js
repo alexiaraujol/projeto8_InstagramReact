f98fa110715a3839e98631f4b2926b4c55bda75a
alexiaraujol <151393882+alexiaraujol@users.noreply.github.com>
2024-12-17 11:21:29 -0300

    Adicionei ações que alteram o nome e a imagem do usuario

--- projeto-instagram-react/src/User.js
+++ projeto-instagram-react/src/User.js
@@ -1,11 +1,28 @@
+import React from "react";
+
 export default function User(){
+
+  const [nome, setNome] = React.useState("");
+  const [imagem, setImagem] = React.useState("assets/img/catanacomics.svg");
+
+
+  function inserirNome(){
+    const nomeDigitado = prompt("Digite o seu nome: ");
+    setNome(nomeDigitado);
+  }
+
+  function inserirImagem(){
+    const imagemAnexada = prompt("Insira a sua imagem aqui: ");
+    setImagem(imagemAnexada)
+  }
+
     return(
         <div class="usuario">
-          <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+          <img onClick={inserirImagem} src={!imagem ? "assets/img/catanacomics.svg" : `${imagem}`} alt="imagem de perfil"/>
           <div class="texto">
             <span>
-              <strong>catanacomics</strong>
-              <ion-icon name="pencil"></ion-icon>
+              <strong>{!nome ? "catanacomics" : `${nome}`}</strong>
+              <ion-icon name="pencil" onClick={inserirNome}></ion-icon>
             </span>
           </div>
         </div>
